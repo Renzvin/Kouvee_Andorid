83be62896da7264ebc93ebe928b4a830f7fa073b
Renzvin <kskkrenzz@gmail.com>
2020-04-09 16:00:12 +0700

    create CRUDS Customer

--- app/src/main/java/com/app/p3l/ui/customer_cs/CustomerCSFragment.java
+++ app/src/main/java/com/app/p3l/ui/customer_cs/CustomerCSFragment.java
@@ -7,29 +7,29 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.app.p3l.Adapter.SectionCustomerPagerAdapter;
 import com.app.p3l.R;
 
 public class CustomerCSFragment extends Fragment {
-    private CustomerCSViewModel customerCSViewModel;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.fragment_cs_customer, container, false);
+        return v;
+    }
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        customerCSViewModel =
-                ViewModelProviders.of(this).get(CustomerCSViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_cs_customer, container, false);
-        final TextView textView = root.findViewById(R.id.text_cs_customer);
-        customerCSViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        SectionCustomerPagerAdapter sectionsPagerAdapter = new SectionCustomerPagerAdapter(getActivity().getApplicationContext(),getFragmentManager());
+        ViewPager viewPager = getView().findViewById(R.id.customer_cs_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
     }
 }
--- app/src/main/java/com/app/p3l/ui/hewan_cs/HewanCSFragment.java
+++ app/src/main/java/com/app/p3l/ui/hewan_cs/HewanCSFragment.java
@@ -5,18 +5,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.app.p3l.Adapter.SectionHewanPagerAdapter;
 import com.app.p3l.R;
-import com.app.p3l.ui.customer_cs.CustomerCSViewModel;
 import com.google.android.material.tabs.TabLayout;
 
 public class HewanCSFragment extends Fragment {
